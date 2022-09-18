bb1ac83f63355c7e1ecf3802e59d3701b037e9b2
obaida <obaidashurbaji99@gmail.com>
2022-09-18 15:25:38 +0200

    add transaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
 	switch (action.type) {
 		case 'DELETE_TRANSACTION':
@@ -8,6 +7,11 @@ export default (state, action) => {
 				transactions: state.transactions.filter(
 					(transaction) => transaction.id !== action.payload
 				),
+			};
+		case 'ADD_TRANSACTION':
+			return {
+				...state,
+				transactions: [action.payload, ...state.transactions],
 			};
 		default:
 			return state;
