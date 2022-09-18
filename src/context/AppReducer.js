5f0efc93ed787ed8feafcbbb8d900010b0ce1716
obaida <obaidashurbaji99@gmail.com>
2022-09-18 14:58:19 +0200

    delete btn

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,14 @@
+import React from 'react';
+
 export default (state, action) => {
 	switch (action.type) {
+		case 'DELETE_TRANSACTION':
+			return {
+				...state,
+				transactions: state.transactions.filter(
+					(transaction) => transaction.id !== action.payload
+				),
+			};
 		default:
 			return state;
 	}
